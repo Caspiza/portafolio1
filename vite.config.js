4db555c757506d8bb5872b89b2b5b06a4095435b
César Castillo <140466759+Caspiza@users.noreply.github.com>
2024-03-18 22:00:38 -0500

    main

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.ttf', '**/*.otf', '**/*.eot'],
 })
